57ebe2ff7b45c8134e1aa96681a8ec09b354bd99
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2022-07-13 21:34:02 -0700

    Get data for sample stock
    Connect to the API, and get the candle data for Apple. Then convert to a dataframe

--- rest.py
+++ rest.py
@@ -1,5 +1,6 @@
 '''     This file is to experiment with the Finnhub.io API      '''
-import requests
+import finnhub
+import pandas as pd
 
 # Specify which secret to read
 SECRET_TYPE = 'sandbox_secret.txt'
@@ -8,3 +9,13 @@ SECRET_TYPE = 'sandbox_secret.txt'
 secret_file = open(SECRET_TYPE, 'r')
 SECRET_API_KEY = secret_file.read()
 secret_file.close()
+
+# Setup client
+fhub_client = finnhub.Client(api_key=SECRET_API_KEY)
+
+# Get stock candle data for Apple
+res = fhub_client.stock_candles('AAPL', 'D', 1590988249, 1591852249)
+
+# Convert to Dataframe
+res_pd = pd.DataFrame(res)
+print(res_pd)
