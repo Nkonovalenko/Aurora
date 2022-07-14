d22822dc0916580869d2adde25c433f88b6b112a
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2022-07-13 21:26:30 -0700

    Read secret key
    Read secret API key from file

--- rest.py
+++ rest.py
@@ -1,2 +1,10 @@
 '''     This file is to experiment with the Finnhub.io API      '''
 import requests
+
+# Specify which secret to read
+SECRET_TYPE = 'sandbox_secret.txt'
+
+# Read in secret API key
+secret_file = open(SECRET_TYPE, 'r')
+SECRET_API_KEY = secret_file.read()
+secret_file.close()
